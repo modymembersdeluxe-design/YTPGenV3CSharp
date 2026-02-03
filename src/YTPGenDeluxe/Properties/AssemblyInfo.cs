f0c13195706ad9f4cd2f52d5a4383c0a96fb6ec2
modymembersdeluxe-design <modymembersdeluxe@gmail.com>
2026-02-03 21:59:06 +0200

    Add SharpDevelop Windows Forms project scaffold

---
+++ src/YTPGenDeluxe/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YTPGen Deluxe")]
+[assembly: AssemblyDescription("Windows 8.1 preview shell for YTPGen Deluxe")]
+[assembly: AssemblyCompany("YTPGen")]
+[assembly: AssemblyProduct("YTPGen Deluxe")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("A23AE4BA-9B38-4C0C-9B13-9F8BC3C2398D")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
